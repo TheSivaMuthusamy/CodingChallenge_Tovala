21c86c76c70e2b50ded67463325677aabb844c63
Siva Muthusamy <s.b.muthusamy@gmail.com>
2017-09-20 17:38:47 -0400

    Production

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,6 @@ var config = {
 				test: /\.(css)$/,
 				loader: 'style-loader!css-loader'
 			},
-			{
-	      		test: /\.(png|jpg|)$/,
-	      		loader: 'url-loader?limit=200000'
-	    	},
 	    	{
 	    		test: /\.svg$/,
 	    		loaders: [
